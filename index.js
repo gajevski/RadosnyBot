1e163ddeab0fb03214ecd14ade37c91862555990
gajevski <mikolajxgajewski@gmail.com>
2024-10-12 19:46:37 +0200

    Prevent from sending messages if there is no new stream

--- index.js
+++ index.js
@@ -1,39 +1,21 @@
 require('dotenv').config();
 const axios = require('axios');
-
 const { Client, GatewayIntentBits } = require('discord.js');
 
 const client = new Client({
-	intents: [
-		GatewayIntentBits.GuildMessages,
-		GatewayIntentBits.MessageContent,
-		GatewayIntentBits.Guilds,
-	],
+  intents: [
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.Guilds,
+  ],
 });
 
 const INTERVAL_TIME = 15 * 60 * 1000;
+let lastStreamId = null;
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
-
-client.on('messageCreate', (message) => {
-    if (message.content.startsWith('!instantgaming')) {
-      
-      const args = message.content.split(' ');
-  
-      const game = args.slice(1).join(' ');
-  
-      if (!game) {
-        message.reply('Podaj nazwę gry, np. `!instantgaming mafia`');
-        return;
-      }
-  
-      message.reply(`https://www.instant-gaming.com/pl/search/?q=${encodeURIComponent(game)}&igr=RadosnyGrajek`);
-    }
-  });
-
-client.login(process.env.CLIENT_TOKEN);
 
 async function checkLiveStream() {
   try {
@@ -50,19 +32,46 @@ async function checkLiveStream() {
     const liveStream = response.data.items;
 
     if (liveStream.length > 0) {
-      console.log(`Kanał rozpoczął transmisję na żywo: https://www.youtube.com/watch?v=${liveStream[0].id.videoId}`);
-      const channel = client.channels.cache.get(process.env.DISCORD_CHANNEL_ID);
-      if (channel) {
-        channel.send(`Kanał rozpoczął transmisję na żywo! Oglądaj tutaj: https://www.youtube.com/watch?v=${liveStream[0].id.videoId}`);
+      const currentStreamId = liveStream[0].id.videoId;
+
+      if (lastStreamId !== currentStreamId) {
+        console.log(`Kanał rozpoczął nową transmisję na żywo: https://www.youtube.com/watch?v=${currentStreamId}`);
+        const channel = client.channels.cache.get(process.env.DISCORD_CHANNEL_ID);
+        
+        if (channel) {
+          channel.send(`Kanał rozpoczął transmisję na żywo! Oglądaj tutaj: https://www.youtube.com/watch?v=${currentStreamId}`);
+        } else {
+          console.error('Nie znaleziono kanału Discord!');
+        }
+
+        lastStreamId = currentStreamId;
       } else {
-        console.error('Nie znaleziono kanału Discord!');
+        console.log('Stream trwa, brak nowej transmisji.');
       }
     } else {
-      console.log('Brak transmisji na żywo.');
+      console.log('Kanał nie jest na żywo.');
+      lastStreamId = null;
     }
   } catch (error) {
     console.error('Błąd podczas sprawdzania transmisji:', error.message);
   }
 }
 
+client.on('messageCreate', (message) => {
+  if (message.content.startsWith('!instantgaming')) {
+    const args = message.content.split(' ');
+
+    const game = args.slice(1).join(' ');
+
+    if (!game) {
+      message.reply('Podaj nazwę gry, np. `!instantgaming mafia`');
+      return;
+    }
+
+    message.reply(`https://www.instant-gaming.com/pl/search/?q=${encodeURIComponent(game)}&igr=RadosnyGrajek`);
+  }
+});
+
 setInterval(checkLiveStream, INTERVAL_TIME);
+
+client.login(process.env.CLIENT_TOKEN);
